db792b361f06444b5e0f513560261804accb6b82
Nikita Plyako <nikito4ka281205@gmail.com>
2024-04-11 19:21:40 +0300

    add k

--- js/random.js
+++ js/random.js
@@ -9,6 +9,9 @@ export const mulmr = () => {
     const numbers = Array.from(SEED.toString(), Number);
     const n3 = numbers[2] || 0;
     const n4 = numbers[3] || 0;
-    const k = 1.0 - n3 * 0.02 - n4 * 0.005 - 0.25;
-    return Math.floor(rand() * k);
+
+    const k1 = 1.0 - n3 * 0.02 - n4 * 0.005 - 0.25;
+    const k2 = 1.0 - n3 * 0.01 - n4 * 0.01 - 0.3;
+
+    return Math.floor(rand() * k2);
 };
